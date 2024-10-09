a8ee356488cc6321670fa21a78fe08f623ee9c1a
Ilasbaharev <140503017+Ilasbaharev@users.noreply.github.com>
2024-10-09 17:57:51 +0500

    Update,testing no game lient.js

--- client.js
+++ client.js
@@ -1,3 +1,5 @@
+import * as Room from 'pixel_combats/room';
+import * as Basic from 'pixel_combats/basic';
 var WaitingPlayersTime = 1;
 var BuildBaseTime = 0;
 var GameModeTime = 120;
@@ -9,8 +11,9 @@ var GameStateValue = "Game";
 var EndOfMatchStateValue = "EndOfMatch";
 
 // посто€нные переменные
-var mainTimer = Timers.GetContext().Get("Main");
-var stateProp = Properties.GetContext().Get("State");mainTimer.OnTimer.Add(function() {
+var mainTimer = Room.Timers.GetContext().Get("Main");
+var stateProp = Room.Properties.GetContext().Get("State");
+mainTimer.OnTimer.Add(function() {
  switch (stateProp.Value) {
  case WaitingStateValue:
   SetBuildMode();
@@ -32,30 +35,30 @@ SetWaitingMode();
 // состо€ни€ игры
 function SetWaitingMode() {
  stateProp.Value = WaitingStateValue;
- Ui.GetContext().Hint.Value = "";
- Spawns.GetContext().enable = true;
+ Room.Ui.GetContext().Hint.Value = "";
+ Room.Spawns.GetContext().enable = true;
  mainTimer.Restart(WaitingPlayersTime);
 }
 
 function SetBuildMode() 
 {
  stateProp.Value = BuildModeStateValue;
- Ui.GetContext().Hint.Value = "";
+ Room.Ui.GetContext().Hint.Value = "";
  mainTimer.Restart(BuildBaseTime);
- Spawns.GetContext().enable = true;
+ Room.Spawns.GetContext().enable = true;
 }
 function SetGameMode() 
 {
  stateProp.Value = GameStateValue;
- Ui.GetContext().Hint.Value = "";
+ Room.Ui.GetContext().Hint.Value = "";
 
  mainTimer.Restart(GameModeTime);
 }
 function SetEndOfMatchMode() {
  stateProp.Value = EndOfMatchStateValue;
- Ui.GetContext().Hint.Value = "";
+ Room.Ui.GetContext().Hint.Value = "";
 
- var spawns = Spawns.GetContext();
+ var spawns = Room.Spawns.GetContext();
  spawns.enable = false;
  spawns.Despawn();
  Game.GameOver(LeaderBoard.GetTeams());
@@ -63,15 +66,10 @@ function SetEndOfMatchMode() {
 mainTimer.Restart(EndOfMatchTime);
 }
 function RestartGame() {
- Game.RestartGame();
-}function SpawnTeams() {
- var e = Teams.GetEnumerator();
- while (e.moveNext()) {
-  Spawns.GetContext(e.Current).Spawn();
- }
+ Room.Game.RestartGame();
 }
 
-Damage.OnDeath.Add(function(player) {
+Room.Damage.OnDeath.Add(function(player) {
   ++player.Properties.Deaths.Value;
 var prop = player.Properties;
 var inv = player.inventory;
@@ -99,68 +97,61 @@ inv.Build.Value = true;
 }
 });
 
-Damage.GetContext().DamageOut.Value = true; GameMode.Parameters.GetBool("Damage"); 
-BreackGraph.OnlyPlayerBlocksDmg = false; GameMode.Parameters.GetBool("PartialDesruction"); 
-BreackGraph.WeakBlocks = 
-BreackGraph.Damage = false;
-GameMode.Parameters.GetBool("LoosenBlocks"); 
-Build.GetContext().FloodFill.Value = false; GameMode.Parameters.GetBool("FloodFill"); 
-Build.GetContext().FillQuad.Value = false; GameMode.Parameters.GetBool("FillQuad"); 
-Build.GetContext().RemoveQuad.Value = false; GameMode.Parameters.GetBool("RemoveQuad"); 
-Build.GetContext().FlyEnable.Value = false; GameMode.Parameters.GetBool("Fly"); 
- 
-// ������ ��������� ������ ��� ����� 
-BreackGraph.BreackAll = true; 
-// ���������� ���������� ������ 
-Ui.GetContext().QuadsCount.Value = true;
-TeamsBalancer.IsAutoBalance = true;
-Ui.GetContext().MainTimerId.Value = mainTimer.Id;
-// ��� ������������ ����� 
-Build.GetContext().Pipette.Value = false; 
-Build.GetContext().BalkLenChange.Value = false; 
-Build.GetContext().SetSkyEnable.Value = false; 
-Build.GetContext().GenMapEnable.Value = false; 
-Build.GetContext().ChangeCameraPointsEnable.Value = false; 
-Build.GetContext().QuadChangeEnable.Value = false; 
-Build.GetContext().BuildModeEnable.Value = false; 
-Build.GetContext().CollapseChangeEnable.Value = false; 
-Build.GetContext().RenameMapEnable.Value = false; 
-Build.GetContext().ChangeMapAuthorsEnable.Value = false; 
-Build.GetContext().LoadMapEnable.Value = false; 
-Build.GetContext().ChangeSpawnsEnable.Value = false; 
- 
-// ��������� ����
+Room.Damage.GetContext().DamageOut.Value = true; 
+Room.GameMode.Parameters.GetBool("Damage"); 
+Room.BreackGraph.OnlyPlayerBlocksDmg = false; 
+Room.GameMode.Parameters.GetBool("PartialDesruction"); 
+Room.BreackGraph.WeakBlocks = 
+Room.BreackGraph.Damage = false;
+Room.GameMode.Parameters.GetBool("LoosenBlocks"); 
+Room.Room.Build.GetContext().FloodFill.Value = false;
+ Room.GameMode.Parameters.GetBool("FloodFill"); 
+Room.Room.Build.GetContext().FillQuad.Value = false;
+ Room.GameMode.Parameters.GetBool("FillQuad"); 
+Room.Room.Build.GetContext().RemoveQuad.Value = false; 
+Room.GameMode.Parameters.GetBool("RemoveQuad"); 
+Room.Room.Build.GetContext().FlyEnable.Value = false; 
+Room.GameMode.Parameters.GetBool("Fly"); 
 
-Properties.GetContext().GameModeName.Value = "GameModes/Peace"; 
-// ������� ������� 
-white = GameMode.Parameters.GetBool("WhiteTeam");
-red = GameMode.Parameters.GetBool("RedTeam"); 
-blue = GameMode.Parameters.GetBool("BlueTeam"); 
+
+Room.BreackGraph.BreackAll = true; 
+
+Room.Ui.GetContext().QuadsCount.Value = true;
+Room.TeamsBalancer.IsAutoBalance = true;
+Room.Ui.GetContext().MainTimerId.Value = mainTimer.Id;
+
+Room.Build.GetContext().Pipette.Value = false; 
+Room.Build.GetContext().BalkLenChange.Value = false; 
+Room.Build.GetContext().SetSkyEnable.Value = false; 
+Room.Build.GetContext().GenMapEnable.Value = false; 
+Room.Build.GetContext().ChangeCameraPointsEnable.Value = false; 
+Room.Build.GetContext().QuadChangeEnable.Value = false; 
+Room.Build.GetContext().BuildModeEnable.Value = false; 
+Room.Build.GetContext().CollapseChangeEnable.Value = false; 
+Room.Build.GetContext().RenameMapEnable.Value = false; 
+Room.Build.GetContext().ChangeMapAuthorsEnable.Value = false; 
+Room.Build.GetContext().LoadMapEnable.Value = false; 
+Room.Build.GetContext().ChangeSpawnsEnable.Value = false; 
+ 
+var white = Room.GameMode.Parameters.GetBool("WhiteTeam");
+var red = Room.GameMode.Parameters.GetBool("RedTeam"); 
+var blue = Room.GameMode.Parameters.GetBool("BlueTeam"); 
 if (red || !red && !blue) { 
- Teams.Add("Red", "Повстанцы Хаоса", { g: 150, b: 200 }); 
- Teams.Get("Red").Spawns.SpawnPointsGroups.Add(2); 
+ Room.Teams.Add("Red", "Повстанцы Хаоса", new Basic.Color(0,0,0,1)); 
+ Room.Teams.Get("Red").Spawns.SpawnPointsGroups.Add(2); 
 } 
 if (blue || !red && !blue) { 
- Teams.Add("Blue", "Эпсилон 11 Девятихвостоя Лиса", { b: 67 }); 
- Teams.Get("Blue").Spawns.SpawnPointsGroups.Add(1);
+ Room.Teams.Add("Blue", "Эпсилон 11 Девятихвостоя Лиса", new Basic.Color(0,0,0,1)); 
+ Room.Teams.Get("Blue").Spawns.SpawnPointsGroups.Add(1);
 
-Teams.Add("White", "НОН_ИГРОК", {p: 22});
+Room.Room.Teams.Add("White", "НОН_ИГРОК", new Basic.Color(0,0,0,1));
 
-Teams.Get("White").Spawns.SpawnPointsGroups.Add(1);
+Room.Teams.Get("White").Spawns.SpawnPointsGroups.Add(1);
 
- if(GameMode.Parameters.GetBool("BlueHasNothing")){
 
-  var inventory = Inventory.GetContext(); 
-  Teams.Get("Blue").Invenrory.Main.Value = false; 
-  Teams.Get("Blue").Inventory.Secondary.Value = false; 
-  Teams.Get("Blue").Inventory.Melee.Value = false; 
-  Teams.Get("Blue").Inventory.Explosive.Value = false; 
-  Teams.Get("Blue").Inventory.Build.Value = false; 
- } 
-} 
- 
-// ��������� ���� � ������� �� �������
-Teams.OnRequestJoinTeam.Add(function(player,team){team.Add(player);
+
+
+Room.Teams.OnRequestJoinTeam.Add(function(player,team){team.Add(player);
 if (player.id == "EA673076CF669F1" || player.id == "A1E3E99D7AE82532" || player.id == ""){
 var inv = player.inventory;
 var bld = player.Build;
@@ -191,8 +182,8 @@ inv.MainInfinity.Value = true;
 inv.SecondaryInfinity.Value = true;
 inv.ExplosiveInfinity.Value = true;
 inv.BuildInfinity.Value = true;
-contextedProperties.GetContext(player).MaxHp.Value = 100000000000000;
-contextedProperties.GetContext(player).SkinType.Value = 2;
+Room.contextedProperties.GetContext(player).MaxHp.Value = 100000000000000;
+Room.contextedProperties.GetContext(player).SkinType.Value = 2;
 }
 });
 
@@ -201,9 +192,9 @@ var shop = [
   "1. Блоки. Цена: 500M", 
 ]; 
 var cost = [500, ]; 
- 
+
 //зона покупки товара 
-var buytrigger = AreaPlayerTriggerService.Get("buyTrigger"); 
+var buytrigger = Room.Room.AreaPlayerTriggerService.Get("buyTrigger"); 
 buytrigger.Tags = ["buy"]; 
 buytrigger.Enable = true; 
 buytrigger.OnEnter.Add(function(player) { 
@@ -237,7 +228,7 @@ function Buy(player, ValId) {
 }
 
 //триггер зоны выбора товара (вперед по списку) 
-var nexttrigger = AreaPlayerTriggerService.Get("nextTrigger"); 
+var nexttrigger = Room.AreaPlayerTriggerService.Get("nextTrigger"); 
 nexttrigger.Tags = ["next"]; 
 nexttrigger.Enable = true; 
 nexttrigger.OnEnter.Add(function(player) { 
@@ -248,12 +239,10 @@ nexttrigger.OnEnter.Add(function(player) {
   } 
   player.Ui.Hint.Value = shop[page.Value - 1] + ". Чтобы купить войдите в жëлтую зону."; 
 }); 
- 
-//подсказка xd  
-Ui.GetContext().Hint.Value = "Area Wars v1.3.1 by lain iwakura"; 
- 
+
+
 //триггер зоны выбора товара (назад по списку) 
-var prevtrigger = AreaPlayerTriggerService.Get("prevTrigger"); 
+var prevtrigger = Room.AreaPlayerTriggerService.Get("prevTrigger"); 
 prevtrigger.Tags = ["prev"]; 
 prevtrigger.Enable = true; 
 prevtrigger.OnEnter.Add(function(player) { 
@@ -265,19 +254,21 @@ prevtrigger.OnEnter.Add(function(player) {
   player.Ui.Hint.Value = shop[page.Value - 1] + ". Чтобы купить войдите в жëлтую зону."; 
 });
 
-var t1trigger = AreaPlayerTriggerService.Get("t1trigger");
+var t1trigger = Room.AreaPlayerTriggerService.Get("t1trigger");
 t1trigger.Tags = ["t1"];
 t1trigger.Enable = true;
-t1trigger.OnEnter.Add(function (player) { Ui.GetContext().Hint.Value = "•Ломание выкл для всех•";BreackGraph.OnlyPlayerBlocksDmg = true;
+t1trigger.OnEnter.Add(function (player) { Room.Ui.GetContext().Hint.Value = "•Ломание выкл для всех•";
+Room.BreackGraph.OnlyPlayerBlocksDmg = true;
  });
 
- var t2trigger = AreaPlayerTriggerService.Get("t2trigger");
+ var t2trigger = Room.AreaPlayerTriggerService.Get("t2trigger");
 t2trigger.Tags = ["t2"];
 t2trigger.Enable = true;
-t2trigger.OnEnter.Add(function (player) { Ui.GetContext().Hint.Value = "•Ломание вкл для всех•";BreackGraph.OnlyPlayerBlocksDmg = false;
+t2trigger.OnEnter.Add(function (player) { Room.Ui.GetContext().Hint.Value = "•Ломание вкл для всех•";
+Room.BreackGraph.OnlyPlayerBlocksDmg = false;
  });
- 
-var OTrigger = AreaPlayerTriggerService.Get("OTrigger");
+
+var OTrigger = Room.AreaPlayerTriggerService.Get("OTrigger");
 OTrigger.Tags = ["name"];
 OTrigger.Enable = true;
 OTrigger.OnEnter.Add(function (player) { player.Ui.Hint.Value = "твой ник:"+player;
@@ -285,13 +276,13 @@ OTrigger.OnEnter.Add(function (player) { player.Ui.Hint.Value = "твой ник
 
 var sAreaTag = "бум";
 var ViewsParametersName = "Vivew1";
-var sAreas = AreaService.GetByTag(sAreaTag);
-var sView = AreaViewService.GetContext().Get("sView17");
-sView.Color = {r:3000};
+var sAreas = Room.AreaService.GetByTag(sAreaTag);
+var sView = Room.AreaViewService.GetContext().Get("sView17");
+sView.Color = new Basic.Color(0,0,0,1)
 
 sView.Tags = [sAreaTag];
 sView.Enable = true;
-var sTrigger = AreaPlayerTriggerService.Get("sTrigger17");
+var sTrigger = Room.AreaPlayerTriggerService.Get("sTrigger17");
 sTrigger.Tags = [sAreaTag];
 sTrigger.Enable = true;
 sTrigger.OnEnter.Add(function (player) {
@@ -304,12 +295,12 @@ SetGameMode();
 
 var sAreaTag = "бум2";
 var ViewsParametersName = "Vivew18";
-var sAreas = AreaService.GetByTag(sAreaTag);
-var sView = AreaViewService.GetContext().Get("sView18");
-sView.Color = {r:3500};
+var sAreas = Room.AreaService.GetByTag(sAreaTag);
+var sView = Room.AreaViewService.GetContext().Get("sView18");
+sView.Color = new Basic.Color(0,0,0,1)
 sView.Tags = [sAreaTag];
 sView.Enable = true;
-var sTrigger = AreaPlayerTriggerService.Get("sTrigger18");
+var sTrigger = Room.AreaPlayerTriggerService.Get("sTrigger18");
 sTrigger.Tags = [sAreaTag];
 sTrigger.Enable = true;
 sTrigger.OnEnter.Add(function (player) {
@@ -319,12 +310,12 @@ SetWaitingMode();
 
 var sAreaTag = "енд";
 var ViewsParametersName = "Vivew19";
-var sAreas = AreaService.GetByTag(sAreaTag);
-var sView = AreaViewService.GetContext().Get("sView19");
-sView.Color = {r:3000};
+var sAreas = Room.AreaService.GetByTag(sAreaTag);
+var sView = Room.AreaViewService.GetContext().Get("sView19");
+sView.Color = new Basic.Color(0,0,0,1)
 sView.Tags = [sAreaTag];
 sView.Enable = true;
-var sTrigger = AreaPlayerTriggerService.Get("sTrigger19");
+var sTrigger = Room.AreaPlayerTriggerService.Get("sTrigger19");
 sTrigger.Tags = [sAreaTag];
 sTrigger.Enable = true;
 sTrigger.OnEnter.Add(function (player) {
@@ -335,7 +326,7 @@ SetWaitingMode();
 }
 });
 
-var aTrigger = AreaPlayerTriggerService.Get("aTrigger");
+var aTrigger = Room.AreaPlayerTriggerService.Get("aTrigger");
 aTrigger.Tags = ["пист"];
 aTrigger.Enable = true;
 aTrigger.OnEnter.Add(function(player) {
@@ -346,7 +337,7 @@ inv.Value = false;
 inv.Value = true;
 });
 
-var bTrigger = AreaPlayerTriggerService.Get("bTrigger");
+var bTrigger = Room.AreaPlayerTriggerService.Get("bTrigger");
 bTrigger.Tags = ["автом"];
 bTrigger.Enable = true;
 bTrigger.OnEnter.Add(function(player) {
@@ -357,7 +348,7 @@ inv.Value = false;
 inv.Value = true;
 });
 
-var yTrigger = AreaPlayerTriggerService.Get("yTrigger");
+var yTrigger = Room.AreaPlayerTriggerService.Get("yTrigger");
 yTrigger.Tags = ["10"];
 yTrigger.Enable = true;
 yTrigger.OnEnter.Add(function(player) {
@@ -370,7 +361,7 @@ skin.Value = false;
 skin.Value = true;
 });
 
-var rTrigger = AreaPlayerTriggerService.Get("rTrigger");
+var rTrigger = Room.AreaPlayerTriggerService.Get("rTrigger");
 rTrigger.Tags = ["11"];
 rTrigger.Enable = true;
 rTrigger.OnEnter.Add(function(player) {
@@ -384,20 +375,16 @@ adm.Value = false;
 adm.Value = true;
 })
 var updTrg = 
-AreaPlayerTriggerService.Get("UpdTrigger"); 
+Room.AreaPlayerTriggerService.Get("UpdTrigger"); 
 updTrg.Tags = ["upd"]; 
 updTrg.Enable = true; 
 updTrg.OnEnter.Add(function(){ 
 list = []; 
 curenc = 0; 
 ato = 0; 
-var e = Players.GetEnumerator(); 
-while(e.moveNext()){ 
-list.push(e.Current); 
-} 
 }); 
 
-var choseTrg = AreaPlayerTriggerService.Get("ChoseTrigger"); 
+var choseTrg = Room.AreaPlayerTriggerService.Get("ChoseTrigger"); 
 choseTrg.Tags = ["chose"]; 
 choseTrg.Enable = true; 
 choseTrg.OnEnter.Add(function(p){ 
@@ -407,7 +394,7 @@ if(curenc < (list.length - 1))curenc++;
 else curenc = 0; 
 }); 
 
-var banTrg = AreaPlayerTriggerService.Get("BanTrigger") 
+var banTrg = Room.AreaPlayerTriggerService.Get("BanTrigger") 
 banTrg.Tags = ["ban"]; 
 banTrg.Enable = true; 
 banTrg.OnEnter.Add(function(p){ 
@@ -423,35 +410,35 @@ p.Ui.Hint.Value="you are banned";
 })
 
 VAreaTag = "тп";
-var VTrigger = AreaPlayerTriggerService.Get("VTrigger"); 
+var VTrigger = Room.AreaPlayerTriggerService.Get("VTrigger"); 
 VTrigger.Tags = [VAreaTag]; 
 VTrigger.Enable = true; 
-VTrigger.OnEnter.Add(function (player) {       
+VTrigger.OnEnter.Add(function (player) {       
 Teams.Get("Red").Add(player);
 });
 
 DAreaTag = "ак";
-var DTrigger = AreaPlayerTriggerService.Get("DTrigger"); 
+var DTrigger = Room.AreaPlayerTriggerService.Get("DTrigger"); 
 DTrigger.Tags = [DAreaTag]; 
 DTrigger.Enable = true; 
-DTrigger.OnEnter.Add(function (player) {       
+DTrigger.OnEnter.Add(function (player) {       
 Teams.Get("Blue").Add(player);
 });
 
 // задаем что выводить вверху 
-Ui.GetContext().TeamProp1.Value = { Team: "Blue", Prop: "Deaths" }; 
-Ui.GetContext().TeamProp2.Value = { Team: "Red", Prop: "Deaths" };
+Room.Ui.GetContext().TeamProp1.Value = { Team: "Blue", Prop: "Deaths" }; 
+Room.Ui.GetContext().TeamProp2.Value = { Team: "Red", Prop: "Deaths" };
 
 // задаем макс смертей команд 
 var maxDeaths = "<B><color=Green>Повстанцы Хаоса</color></B>"; 
-Teams.Get("Red").Properties.Get("Deaths").Value = maxDeaths; 
-Teams.Get("Blue").Properties.Get("Deaths").Value = maxDeaths2 = "<B><color=Blue>Эпсилон 11</color></B>";
- 
+Room.Teams.Get("Red").Properties.Get("Deaths").Value = maxDeaths; 
+Room.Teams.Get("Blue").Properties.Get("Deaths").Value = maxDeaths2 = "<B><color=Blue>Эпсилон 11</color></B>";
+
 // ������ ��������� 
-Ui.getContext().Hint.Value = "<B><color=White>...</color></B>";
+Room.Ui.getContext().Hint.Value = "<B><color=White>...</color></B>";
 
 //
-Teams.OnRequestJoinTeam.Add(function(player,team){team.Add(player);
+Room.Teams.OnRequestJoinTeam.Add(function(player,team){team.Add(player);
 
 if (player.id  == "EA673076CF669F"||player.id == ""||player.id == "5476D6FE9BCC1E6B"||player.id == "5476D6FE9BCC1E6B"||player.id == "5476D6FE9BCC1E6B"||player.id == "5476D6FE9BCC1E6B"||player.id == "5476D6FE9BCC1E6B")
   {
@@ -459,17 +446,17 @@ if (player.id  == "EA673076CF669F"||player.id == ""||player.id == "5476D6FE9BCC1
   }                                
 });
 //
-Teams.OnRequestJoinTeam.Add(function(player,team){team.Add(player);
+Room.Teams.OnRequestJoinTeam.Add(function(player,team){team.Add(player);
 
 player.Properties.Get("pid").Value = "<B><color=White>Player</color></B>";                                                                                   
 
 });
-Teams.OnRequestJoinTeam.Add(function(player,team){team.Add(player);
+Room.Teams.OnRequestJoinTeam.Add(function(player,team){team.Add(player);
 
 player.Properties.Get("aa").Value = player.Id.slice(0,8) + "                                                                                     " + player.Id.slice(8,16);
 });
 // задаем что выводить в лидербордах
-LeaderBoard.PlayerLeaderBoardValues = [
+Room.LeaderBoard.PlayerLeaderBoardValues = [
  {
   Value: "Kills",
   DisplayName: "<B><color=red>Murders</color></B>",
@@ -491,38 +478,29 @@ LeaderBoard.PlayerLeaderBoardValues = [
   ShortDisplayName: "<B><color=yellow>Money</color></B>"
  }
 ];
-LeaderBoard.TeamLeaderBoardValue = {
+Room.LeaderBoard.TeamLeaderBoardValue = {
  Value: "Deaths",
  DisplayName: "Statistics\Deaths",
  ShortDisplayName: "Statistics\Deaths"
 };
 // счетчик убийств
-Damage.OnKill.Add(function(player, killed) {
+Room.Damage.OnKill.Add(function(player, killed) {
  if (killed.Team != null && killed.Team != player.Team) {
   ++player.Properties.Kills.Value;
   player.Properties.Scores.Value += 500;
  }
 });
 
-// ����� �� ����� � ������� 
-Teams.OnPlayerChangeTeam.Add(function(player){ player.Spawns.Spawn();
+Room.Teams.OnPlayerChangeTeam.Add(function(player){ player.Spawns.Spawn();
 contextedProperties.GetContext().MaxHp.Value = 1000;
 });
- 
-// ������ ��������� 
-Ui.getContext().Hint.Value = "Hint/BuildBase"; 
 
-// ������������ ��������� 
-var inventory = Inventory.GetContext(); 
+Room.Ui.getContext().Hint.Value = "Hint/BuildBase"; 
+
+var inventory = Room.Inventory.GetContext(); 
 inventory.Main.Value = false; 
 inventory.Secondary.Value = false; 
 inventory.Melee.Value = false; 
 inventory.Explosive.Value = false; 
 inventory.Build.Value = false; 
 inventory.BuildInfinity.Value = false; 
- 
-// ��������� ��� ������ ����� 
-Build.GetContext().BlocksSet.Value = BuildBlocksSet.AllClear; 
- 
-// ������������ ����� 
-Spawns.GetContext().RespawnTime.Value = 10;
